36ceec5eb02ee7f90b05cb9d6a1ad3e6ceb76d8c
RAQKID <officialraqkid@gmail.com>
2024-07-20 17:55:18 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -14,24 +14,24 @@ module.exports = {
     },
     keepAlive: parseBoolean(process.env.KEEP_ALIVE) || true, // for https://replit.com keep alive bot 24/7
     searchEngine: process.env.SEARCH_ENGINE || "ytmsearch", // ytsearch = youtube, scsearch = soundcloud, spsearch = spotify,
-    maxPlaylistSize: parseInt(process.env.MAX_PLAYLIST_SIZE) || 150,
-    botStatus: process.env.BOT_STATUS || '', // online, idle, dnd, invisible
+    maxPlaylistSize: parseInt(process.env.MAX_PLAYLIST_SIZE) || 2500,
+    botStatus: process.env.BOT_STATUS || 'dnd', // online, idle, dnd, invisible
     botActivity: process.env.BOT_ACTIVITY || '', // set the bot activity
     botActivityType: parseInt(process.env.BOT_ACTIVITY_TYPE || '0'), // 0 to 5 get more info - https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-types
-    maxQueueSize: parseInt(process.env.MAX_QUEUE_SIZE) || 350,
+    maxQueueSize: parseInt(process.env.MAX_QUEUE_SIZE) || 5000,
     owners: JSON.parse(process.env.OWNER_IDS || '[]'),
     clientId: process.env.CLIENT_ID,
     guildId: process.env.GUILD_ID,
     logChannelId: process.env.LOG_CHANNEL_ID || '',
     links: {
-        img: process.env.IMG_LINK || 'https://i.ibb.co/wz2rvDv/istockphoto-1256174227-612x612.jpg',
+        img: process.env.IMG_LINK || 'https://i.ibb.co/fYnpJv9/images-27.jpg',
     },
     icons: {
-        youtube: 'https://i.ibb.co/wz2rvDv/istockphoto-1256174227-612x612.jpg',
-        spotify: 'https://i.ibb.co/wz2rvDv/istockphoto-1256174227-612x612.jpg',
-        soundcloud: 'https://i.ibb.co/wz2rvDv/istockphoto-1256174227-612x612.jpg',
-        applemusic: 'https://i.ibb.co/wz2rvDv/istockphoto-1256174227-612x612.jpg',
-        deezer: 'https://i.ibb.co/wz2rvDv/istockphoto-1256174227-612x612.jpg',
+        youtube: 'https://i.ibb.co/G7GXqdL/istockphoto-1256174227-612x612-removebg-preview.jpg',
+        spotify: 'https://i.ibb.co/G7GXqdL/istockphoto-1256174227-612x612-removebg-preview.jpg',
+        soundcloud: 'https://i.ibb.co/G7GXqdL/istockphoto-1256174227-612x612-removebg-preview.jpg',
+        applemusic: 'https://i.ibb.co/G7GXqdL/istockphoto-1256174227-612x612-removebg-preview.jpg',
+        deezer: 'https://i.ibb.co/G7GXqdL/istockphoto-1256174227-612x612-removebg-preview.jpg',
     },
     production: parseBoolean(process.env.PRODUCTION) || true,
     lavalink: [
